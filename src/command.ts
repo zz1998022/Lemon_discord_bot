615a8e9aac7117845456bdee5e8c70cc158d1ff8
zz1998022 <1074045407@qq.com>
2022-04-06 20:51:02 +0800

    添加:完成指令注册

---
+++ src/command.ts
@@ -0,0 +1,48 @@
+import { config } from "../config";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+
+const { BOT_TOKEN } = config;
+
+// 注册指令
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+  {
+    name: "say",
+    description: "say something",
+  },
+  {
+    name: "ts",
+    description: "translate something",
+  },
+];
+
+const rest = new REST({ version: "9" }).setToken(BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest
+      .put(
+        Routes.applicationGuildCommands(
+          "905298955011784735",
+          "905296187207344158"
+        ),
+        { body: commands }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.requestBody);
+      });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
